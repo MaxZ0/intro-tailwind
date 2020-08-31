528a52d37430245a77e24089d7c157f51f786b31
MaxZ0 <maxkrohn@hotmail.com>
2020-08-31 11:20:47 +0200

    almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
   purge: [],
   theme: {
+    colors: {
+      black: "#121212",
+      gray: "#282828",
+    },
     extend: {},
   },
   variants: {},
